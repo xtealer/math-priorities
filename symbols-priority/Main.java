085e77e7217b5890620610df349fb7291c948f45
Enrique Shunnar <xtealer00@gmail.com>
2021-07-11 15:24:57 -0500

    Add files via upload

---
+++ symbols-priority/Main.java
@@ -0,0 +1,7 @@
+import java.io.*;
+
+class Main {
+	public static void main(String[] args){
+		System.out.print(1+1+1+1+1*0+1);
+	}
+}
---
+++ symbols-priority/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+	int result = 1+1+1+1+1*0+1;
+	printf("%d",result);
+	return 0;
+}
